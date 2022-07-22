939970086e5eb995b84f1865b391d407d8454712
Lucas Barrios <lucasbarrios521@gmail.com>
2022-07-22 15:54:17 -0300

    Nueva version desafio

--- js/desafio.js
+++ js/desafio.js
@@ -14,10 +14,16 @@ do{
 let IngresarNumero = 0
 
 do{
-    IngresarNumero = prompt(" Ingrese un numero ")
-    for( let i = 0 ; i <= IngresarNumero ; i++){
-        alert(i)   
-    }
+    IngresarNumero = prompt(" Ingrese un numero (o ESC para salir). ")
+    if(isNaN(IngresarNumero) && IngresarNumero != "ESC"){
+        alert( "(" + IngresarNumero + ")" + " no es un numero.")
+        }else{
+            for( let i = 0 ; i <= IngresarNumero ; i++){
+            alert(i)  
+            }
+        }
 }while(IngresarNumero != "ESC"){
     alert("Saliendo...")
 }
+
+
